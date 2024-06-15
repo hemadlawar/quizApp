f117cc0b5f5fac348f3501867cb87fbcb4451c9c
hemaddlawar <hema.200219627512@gmail.com>
2024-06-15 15:50:42 +0300

    render the questions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import { useSelector, useDispatch } from "react-redux";
 import { increment, reset } from "../src/REDUX/slice";
 function App() {
+  const dispatch = useDispatch();
   const states = useSelector((state) => {
     return state;
   });
-  console.log(states);
-  return <div>hello world!</div>;
+
+  const layout = states.questions.map((question) => {
+    return (
+      <div>
+        <p>{question.title}</p>
+        <ul>
+          <li>{question.options.a}</li>
+          <li>{question.options.b}</li>
+          <li>{question.options.c}</li>
+          <li>{question.options.d}</li>
+        </ul>
+      </div>
+    );
+  });
+
+  return <div>{layout}</div>;
 }
 
 export default App;
